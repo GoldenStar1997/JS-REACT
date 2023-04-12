fd42df7ee71758a9693874e3e818a7760ba1984a
GoldenStar1997 <golden.starr1997@gmail.com>
2023-04-12 10:21:19 +0100

    fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MainLayout from '../components/layout/MainLayout';
 
 function Home() {
-  window.document.title = 'React App — Home';
+  window.document.title = 'React App-Home';
 
   return (
     <MainLayout>
